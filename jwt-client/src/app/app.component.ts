8a0f50c7a6242a732e08bf39d9c4c3460b59f7c9
tdtc-hrb <tdtc.hrb@gmail.com>
2024-11-22 10:08:52 -0800

    fixed: "TS-996008"
    Important: In Angular versions before 19.0.0, the `standalone` option defaults to `false`.

--- jwt-client/src/app/app.component.ts
+++ jwt-client/src/app/app.component.ts
@@ -5,7 +5,8 @@ import { SigninService } from './signin/signin.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent implements OnInit {
   title = 'UserDetails-jwt-client';
